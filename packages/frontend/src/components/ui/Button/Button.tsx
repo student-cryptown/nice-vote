abdf0b4f85130a3c3410090077e9d7350d084471
inaridiy <inainainariinainari@gmail.com>
2023-04-16 02:25:37 +0900

    feat: btn

--- packages/frontend/src/components/ui/Button/Button.tsx
+++ packages/frontend/src/components/ui/Button/Button.tsx
@@ -1,10 +1,10 @@
-import { VariantProps, cva } from "class-variance-authority"
-import * as React from "react"
+import { VariantProps, cva } from "class-variance-authority";
+import * as React from "react";
 
-import { cn } from "@/utils"
+import { cn } from "@/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800",
+  "inline-flex items-center justify-center rounded-md text-sm font-bold  transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800",
   {
     variants: {
       variant: {
@@ -22,20 +22,25 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 py-2 px-4",
-        sm: "h-9 px-2 rounded-md",
+        sm: "h-9 px-3 rounded-md",
         lg: "h-11 px-8 rounded-md",
+      },
+      shadow: {
+        default: "",
+        lg: "drop-shadow-lg",
       },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      shadow: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> { }
+    VariantProps<typeof buttonVariants> {}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, ...props }, ref) => {
@@ -45,9 +50,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
