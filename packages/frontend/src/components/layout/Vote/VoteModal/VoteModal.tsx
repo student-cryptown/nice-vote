b37cf67c552f489864b62a9ef3c83b1a18bd0316
inaridiy <inainainariinainari@gmail.com>
2023-04-16 03:15:20 +0900

    feat

--- packages/frontend/src/components/layout/Vote/VoteModal/VoteModal.tsx
+++ packages/frontend/src/components/layout/Vote/VoteModal/VoteModal.tsx
@@ -8,8 +8,12 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/Modal";
+import { useContractAddress } from "@/hooks/useContractAddress";
 import { useVote } from "@/hooks/useVote";
+import { Hex } from "@/types";
+import { BigNumber } from "ethers";
 import React, { useState } from "react";
+import { useContractWrite, usePrepareContractWrite } from "wagmi";
 
 export const VoteModal: React.FC<{
   open: boolean;
@@ -17,9 +21,33 @@ export const VoteModal: React.FC<{
   voteHash: string;
   voteOption?: number;
 }> = ({ open, onClose, voteHash, voteOption }) => {
+  const contracts = useContractAddress();
   const vote = useVote(voteHash);
   const [intmaxAddress, setIntmaxAddress] = useState("");
   const [castVotes, setCastVotes] = useState(0);
+
+  const isValid =
+    !!intmaxAddress &&
+    !!castVotes &&
+    typeof voteOption === "number" &&
+    typeof castVotes === "number";
+
+  const { config } = usePrepareContractWrite({
+    address: contracts?.VOTE.address,
+    abi: contracts?.VOTE.abi,
+    functionName: "vote",
+    args: [
+      voteHash as Hex,
+      intmaxAddress as Hex,
+      BigNumber.from(castVotes),
+      BigNumber.from(voteOption || 0),
+    ],
+    enabled: isValid,
+  });
+  console.log(isValid);
+  const { data, isLoading, isSuccess, write } = useContractWrite(config);
+
+  console.log({ intmaxAddress, castVotes, voteOption, isValid });
   return (
     <AlertDialog open={open} onOpenChange={() => onClose()}>
       <AlertDialogContent>
@@ -28,23 +56,21 @@ export const VoteModal: React.FC<{
             vote?.voteOptions[voteOption || 0]
           }"?`}</AlertDialogTitle>
 
-          <div className="grid w-full items-center gap-1.5 my-5">
+          <div className="grid w-full items-center gap-1.5 pt-4">
             <Label>IntmaxAddressForVote</Label>
             <Input
               placeholder="0x00"
               type="text"
-              value={intmaxAddress}
               onChange={(e) => {
                 setIntmaxAddress(e.target.value);
               }}
             />
           </div>
-          <div className="grid w-full items-center gap-1.5 my-5">
+          <div className="grid w-full items-center gap-1.5 py-4">
             <Label>Cast Votes</Label>
             <Input
               placeholder="1"
               type="number"
-              value={castVotes}
               onChange={(e) => {
                 setCastVotes(parseInt(e.target.value));
               }}
@@ -53,7 +79,14 @@ export const VoteModal: React.FC<{
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Vote 👍️</AlertDialogAction>
+          <AlertDialogAction
+            disabled={!isValid}
+            onClick={() => {
+              return write && write();
+            }}
+          >
+            Vote 👍️
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
