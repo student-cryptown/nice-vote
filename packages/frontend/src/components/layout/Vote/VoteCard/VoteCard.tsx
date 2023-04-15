6b4d6f873ac02ce62e6cb80317551ab7af23d295
inaridiy <inainainariinainari@gmail.com>
2023-04-16 03:32:46 +0900

    feat

--- packages/frontend/src/components/layout/Vote/VoteCard/VoteCard.tsx
+++ packages/frontend/src/components/layout/Vote/VoteCard/VoteCard.tsx
@@ -4,34 +4,44 @@ import { extractTextBeforeNewline, sumArray } from "@/utils";
 import Link from "next/link";
 
 export const VoteCard = ({
-  id, showOpenOnly = false, showFinalizedOnly = false
-}: { id: string, showOpenOnly?: boolean, showFinalizedOnly?: boolean }) => {
-
-  const vote = useVote(id)
+  id,
+  showOpenOnly = false,
+  showFinalizedOnly = false,
+}: {
+  id: string;
+  showOpenOnly?: boolean;
+  showFinalizedOnly?: boolean;
+}) => {
+  const vote = useVote(id);
 
   const nowDatetime = new Date().getTime();
 
   if (!vote) return <></>;
-  if (showOpenOnly && vote.voteEnd.toNumber() * 1000 < nowDatetime) return <></>;
-  if (showFinalizedOnly && vote.voteEnd.toNumber() * 1000 > nowDatetime) return <></>;
+  if (showOpenOnly && vote.voteEnd.toNumber() * 1000 < nowDatetime)
+    return <></>;
+  if (showFinalizedOnly && vote.voteEnd.toNumber() * 1000 > nowDatetime)
+    return <></>;
 
-  return (<Link href={"/vote/" + id}>
-    <div className="rounded-md border-2 border-gray-800 pt-4 cursor-pointer hover:-translate-y-1">
-      <div className="px-4 pb-3">
-        <TypographyH3 className="line-clamp-1">
-          {extractTextBeforeNewline(vote.description)}
-        </TypographyH3>
-        <TypographyP className="mt-2 line-clamp-3">
-          {vote.description}
-        </TypographyP>
-        <div className="flex items-center gap-2 justify-end mt-2">
-          <div className="flex-1 flex items-center">
-            <TypographyP>
-              {"Total votes cast : " + sumArray(vote.voteCounts.map((count) => count.toNumber()))}
-            </TypographyP>
+  return (
+    <Link href={"/vote/" + id}>
+      <div className="rounded-md border-2 h-full border-gray-800 pt-4 cursor-pointer hover:-translate-y-1">
+        <div className="px-4 pb-3">
+          <TypographyH3 className="line-clamp-1">
+            {extractTextBeforeNewline(vote.description)}
+          </TypographyH3>
+          <TypographyP className="mt-2 line-clamp-3">
+            {vote.description}
+          </TypographyP>
+          <div className="flex items-center gap-2 justify-end mt-2">
+            <div className="flex-1 flex items-center">
+              <TypographyP>
+                {"Total votes cast : " +
+                  sumArray(vote.voteCounts.map((count) => count.toNumber()))}
+              </TypographyP>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </Link>)
-}
+    </Link>
+  );
+};
--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,8 +1,14 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger, TypographyH2, VoteCard } from "@/components";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+  TypographyH2,
+  VoteCard,
+} from "@/components";
 import { useVoteHashes } from "@/hooks/useVoteHashes";
 
 export default function Home() {
-
   const voteHashes = useVoteHashes();
   return (
     <>
@@ -12,16 +18,15 @@ export default function Home() {
           <Tabs defaultValue="open" className="max-w-full mb-6">
             <TabsContent value="open" className="p-0 rounded-none border-none">
               <div className="text-center my-6">
-                <TypographyH2>
-                  Open Votes
-                </TypographyH2>
+                <TypographyH2>Open Votes</TypographyH2>
               </div>
             </TabsContent>
-            <TabsContent value="finalized" className="p-0 rounded-none border-none">
+            <TabsContent
+              value="finalized"
+              className="p-0 rounded-none border-none"
+            >
               <div className="text-center my-6">
-                <TypographyH2>
-                  Finalized Votes
-                </TypographyH2>
+                <TypographyH2>Finalized Votes</TypographyH2>
               </div>
             </TabsContent>
             <div className="flex justify-center md:justify-end w-full mb-4">
@@ -31,37 +36,69 @@ export default function Home() {
               </TabsList>
             </div>
             <TabsContent value="open" className="p-0 rounded-none border-none">
-              <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-                {voteHashes && voteHashes.length != 0 ? <>
-                  {voteHashes.slice().reverse().map((voteHash, index) => {
-                    return (<VoteCard id={voteHash} key={index} showOpenOnly />)
-                  })}
-
-                </> : <>
-                  {[0, 0, 0, 0].map((_, _v) => {
-                    return (<div className="animate-pulse w-full h-40 bg-gray-100" key={_v + "loading"} />)
-                  })}
-                </>}
+              <div className="grid gap-5 md:grid-cols-2 content-stretch lg:grid-cols-2 xl:grid-cols-3 grid-rows-1 align-content-stretch">
+                {voteHashes && voteHashes.length != 0 ? (
+                  <>
+                    {voteHashes
+                      .slice()
+                      .reverse()
+                      .map((voteHash, index) => {
+                        return (
+                          <VoteCard id={voteHash} key={index} showOpenOnly />
+                        );
+                      })}
+                  </>
+                ) : (
+                  <>
+                    {[0, 0, 0, 0].map((_, _v) => {
+                      return (
+                        <div
+                          className="animate-pulse w-full h-40 bg-gray-100"
+                          key={_v + "loading"}
+                        />
+                      );
+                    })}
+                  </>
+                )}
               </div>
             </TabsContent>
-            <TabsContent value="finalized" className="p-0 rounded-none border-none">
+            <TabsContent
+              value="finalized"
+              className="p-0 rounded-none border-none"
+            >
               <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-                {voteHashes && voteHashes.length != 0 ? <>
-
-                  {voteHashes.slice().reverse().map((voteHash, index) => {
-                    return (<VoteCard id={voteHash} key={index} showFinalizedOnly />)
-                  })}
-
-                </> : <>
-                  {[0, 0, 0, 0].map((_, _v) => {
-                    return (<div className="animate-pulse w-full h-40 bg-gray-100" key={_v + "loading"} />)
-                  })}
-                </>}
+                {voteHashes && voteHashes.length != 0 ? (
+                  <>
+                    {voteHashes
+                      .slice()
+                      .reverse()
+                      .map((voteHash, index) => {
+                        return (
+                          <VoteCard
+                            id={voteHash}
+                            key={index}
+                            showFinalizedOnly
+                          />
+                        );
+                      })}
+                  </>
+                ) : (
+                  <>
+                    {[0, 0, 0, 0].map((_, _v) => {
+                      return (
+                        <div
+                          className="animate-pulse w-full h-40 bg-gray-100"
+                          key={_v + "loading"}
+                        />
+                      );
+                    })}
+                  </>
+                )}
               </div>
             </TabsContent>
           </Tabs>
         </div>
       </div>
     </>
-  )
+  );
 }
